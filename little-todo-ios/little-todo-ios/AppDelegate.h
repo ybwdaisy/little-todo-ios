609ec98b7796e061a0d4f521a7ab4fca5dc64ed0
yangbiwan <yangbiwan@lasoplanet.com>
2020-07-04 16:14:12 +0800

    init with objective-c

---
+++ little-todo-ios/little-todo-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  little-todo-ios
+//
+//  Created by ybwdaisy on 2020/7/4.
+//  Copyright © 2020 ybwdaisy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
