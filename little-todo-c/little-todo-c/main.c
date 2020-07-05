4da66384738009c860a3c179c3da3df5080856c4
yangbiwan <yangbiwan@lasoplanet.com>
2020-07-05 09:53:58 +0800

    merge branch

---
+++ little-todo-c/little-todo-c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  little-todo-c
+//
+//  Created by ybwdaisy on 2020/7/5.
+//  Copyright © 2020 ybwdaisy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
