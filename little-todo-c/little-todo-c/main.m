3c67e95ca87e5f77a8181b82e116af3fc6caac18
yangbiwan <yangbiwan@lasoplanet.com>
2020-07-16 09:39:32 +0800

    clean

--- little-todo-c/little-todo-c/main.m
+++ little-todo-c/little-todo-c/main.m
@@ -6,18 +6,7 @@
 //  Copyright © 2020 ybwdaisy. All rights reserved.
 //
 
-#import "Person.h"
-
 int main()
 {
-    Person *p1 = [Person new];
-    
-    [p1 setName:@"向博"];
-    [p1 setAge:30];
-    [p1 setHeight:175.0f];
-    [p1 setWeight:70.0f];
-    
-    NSLog(@"我是: %@, age: %d, height: %f, weight: %f", [p1 name], [p1 age], [p1 height], [p1 weight]);
-    
     return 0;
 }
