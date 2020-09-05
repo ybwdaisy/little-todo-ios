82ed7d03e7a110091449e5dc00434a3fc670d0d1
yangbiwan <yangbiwan@lasoplanet.com>
2020-09-05 20:54:09 +0800

    add .gitignore, relate to https://github.com/github/gitignore/blob/master/Objective-C.gitignore

--- little-todo-ios/little-todo-ios/main.m
+++ little-todo-ios/little-todo-ios/main.m
@@ -9,48 +9,12 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-@interface Person : NSObject
-{
-    NSString *name;
-    int age;
-    float height;
-}
-- (void) run;
-- (void) eat:(NSString *) foodName;
-- (int) sum:(int) num1 :(int) num2;
-@end
-
-@implementation Person
-
-- (void) run {
-    NSLog(@"Execution run function");
-}
-
-- (void) eat:(NSString *) foodName {
-    NSLog(@"Execution eat function, args foodName is %@", foodName);
-}
-
-- (int) sum:(int) num1 :(int) num2 {
-    return num1 + num2;
-}
-
-@end
-
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
         // Setup code that might create autoreleased objects goes here.
         appDelegateClassName = NSStringFromClass([AppDelegate class]);
     }
-    
-    Person *p1 = [Person new];
-    
-    [p1 run];
-    
-    [p1 eat:@"apple"];
-    
-    int sum = [p1 sum:10 :20];
-    NSLog(@"sum is %d", sum);
     
     return UIApplicationMain(argc, argv, nil, appDelegateClassName);
 }
