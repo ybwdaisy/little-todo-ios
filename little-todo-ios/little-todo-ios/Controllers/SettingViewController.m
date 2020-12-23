0929aa84f6152c2bddf3ace91f5be71f170b58c4
yangbiwan <yangbiwan@lasoplanet.com>
2020-12-23 20:07:18 +0800

    更新tabbar图标

--- little-todo-ios/little-todo-ios/Controllers/SettingViewController.m
+++ little-todo-ios/little-todo-ios/Controllers/SettingViewController.m
@@ -8,7 +8,7 @@
 
 #import "SettingViewController.h"
 
-@interface SettingViewController () <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+@interface SettingViewController ()
 
 @end
 
@@ -28,43 +28,6 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
-    flowLayout.minimumLineSpacing = 10;
-    flowLayout.minimumInteritemSpacing = 10;
-    flowLayout.itemSize = CGSizeMake((self.view.frame.size.width - 10) / 2, 300);
-    
-    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:flowLayout];
-    
-    collectionView.dataSource = self;
-    collectionView.delegate = self;
-    
-    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"UICollectionViewCell"];
-    
-    [self.view addSubview:collectionView];
-    
-}
-
-
-- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return 20;
-}
-
-// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
-- (__kindof UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"UICollectionViewCell" forIndexPath:indexPath];
-    
-    cell.backgroundColor = [UIColor lightGrayColor];
-    
-    return cell;;
-}
-
-- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    if (indexPath.item % 3 == 0) {
-        return CGSizeMake(self.view.frame.size.width, 100);
-    } else {
-        return CGSizeMake((self.view.frame.size.width - 10) / 2 , 300);
-    }
 }
 
 
