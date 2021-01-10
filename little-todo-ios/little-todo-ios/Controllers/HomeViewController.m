fce47afe6f02cfeb05f5e49f7ab31b163f346aad
ybwdaisy <ybw_daisy@163.com>
2021-01-10 23:05:49 +0800

    add todo popover modal

--- little-todo-ios/little-todo-ios/Controllers/HomeViewController.m
+++ little-todo-ios/little-todo-ios/Controllers/HomeViewController.m
@@ -8,6 +8,7 @@
 
 #import "HomeViewController.h"
 #import "TDTableViewCell.h"
+#import "SettingViewController.h"
 
 @interface HomeViewController ()<UITableViewDelegate>
 
@@ -45,13 +46,18 @@
     
     // 添加按钮
     FAKFontAwesome *plusIcon = [FAKFontAwesome plusIconWithSize:20];
+    [plusIcon addAttribute:NSForegroundColorAttributeName value:[UIColor whiteColor]];
     UIImageView *plusIconImageView = [[UIImageView alloc] initWithImage:[plusIcon imageWithSize:CGSizeMake(20, 20)]];
     
-    self.plusButtonContainerView = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width, self.view.frame.size.height, 60, 60)];
+    self.plusButtonContainerView = [[UIView alloc] initWithFrame:CGRectMake(self.view.frame.size.width, self.view.frame.size.height, 50, 50)];
     self.plusButtonContainerView.backgroundColor = UIColor.orangeColor;
-    self.plusButtonContainerView.layer.cornerRadius = 30;
+    self.plusButtonContainerView.layer.cornerRadius = 25;
     
     plusIconImageView.center = CGPointMake(self.plusButtonContainerView.frame.size.width / 2, self.plusButtonContainerView.frame.size.height / 2);
+    
+    // 添加事件
+    UITapGestureRecognizer *tapAddTodo = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(addTodo)];
+    [self.plusButtonContainerView addGestureRecognizer:tapAddTodo];
     
     [self.plusButtonContainerView addSubview:plusIconImageView];
     [self.view addSubview:self.plusButtonContainerView];
@@ -112,7 +118,44 @@
 
 - (void)viewSafeAreaInsetsDidChange NS_REQUIRES_SUPER API_AVAILABLE(ios(11.0), tvos(11.0)) {
     [super viewSafeAreaInsetsDidChange];
-    [self.plusButtonContainerView setFrame:CGRectMake(self.view.frame.size.width - 80, self.view.frame.size.height - self.view.safeAreaInsets.bottom - 80, 60, 60)];
+    [self.plusButtonContainerView setFrame:CGRectMake(self.view.frame.size.width - 80, self.view.frame.size.height - self.view.safeAreaInsets.bottom - 80, 50, 50)];
+}
+
+- (void)addTodo {
+    UIViewController *modalViewController = [[UIViewController alloc]init];
+    
+    UIView *modalView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    modalView.backgroundColor = [UIColor whiteColor];
+
+    UIView *headerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 50)];
+    
+    UIButton *headerLeftButton = [[UIButton alloc]initWithFrame:CGRectMake(20, 15, 50, 50)];
+    [headerLeftButton setTitle:@"取消" forState:UIControlStateNormal];
+    [headerLeftButton setTitleColor:[UIColor blueColor] forState:UIControlStateNormal];
+    [headerView addSubview:headerLeftButton];
+    
+    UILabel *headerTitle = [[UILabel alloc]initWithFrame:CGRectMake(20 + headerLeftButton.frame.size.width, 15, self.view.frame.size.width - 140, 50)];
+    headerTitle.text = @"新建代办";
+    headerTitle.font = [UIFont boldSystemFontOfSize:20];
+    [headerTitle setTextAlignment:NSTextAlignmentCenter];
+    [headerView addSubview:headerTitle];
+    
+    UIButton *headerRightButton = [[UIButton alloc]initWithFrame:CGRectMake(20 + headerLeftButton.frame.size.width + headerTitle.frame.size.width, 15, 50, 50)];
+    [headerRightButton setTitle:@"确认" forState:UIControlStateNormal];
+    [headerRightButton setTitleColor:[UIColor lightGrayColor] forState:UIControlStateNormal];
+    [headerView addSubview:headerRightButton];
+    
+    [modalView addSubview:headerView];
+    
+    UITextView *textView = [[UITextView alloc]initWithFrame:CGRectMake(20, 90, self.view.frame.size.width - 40, self.view.frame.size.height) textContainer:nil];
+    textView.text = @"请输入";
+    textView.editable = YES;
+    textView.font = [UIFont systemFontOfSize:16];
+    [modalView addSubview:textView];
+    
+    [modalViewController.view addSubview:modalView];
+    modalViewController.modalPresentationStyle = UIModalPresentationPopover;
+    [self presentViewController:modalViewController animated:YES completion:nil];
 }
 
 @end
